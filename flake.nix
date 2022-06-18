ea9db1c6b583fc62e71f0c23712248031e8ea250
mitchmindtree <mail@mitchellnordine.com>
2022-06-18 14:03:03 +1000

    Remove unused env assignment. Simplify outputs formatting.

--- flake.nix
+++ flake.nix
@@ -30,70 +30,67 @@
   };
 
   outputs = inputs: let
-    out = system: let
-      env = import ./env { inherit inputs; inherit system; };
-      quark-lib = import ./quark/lib.nix;
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
-      ghcWithTidal = pkgs.haskellPackages.ghcWithPackages (p: [ p.tidal ]);
-    in rec {
-      packages.${system} = rec {
-        # SuperCollider quarks that are necessary for Tidal.
-        dirt-samples = pkgs.callPackage ./quark/dirt-samples.nix {
-          inherit (quark-lib) mkQuark;
-          inherit (inputs) dirt-samples-src;
-        };
-        vowel = pkgs.callPackage ./quark/vowel.nix {
-          inherit (quark-lib) mkQuark;
-          inherit (inputs) vowel-src;
-        };
-        superdirt = pkgs.callPackage ./quark/superdirt.nix {
-          inherit (quark-lib) mkQuark;
-          inherit (inputs) superdirt-src;
-          dependencies = [ dirt-samples vowel ];
-        };
-
-        # Run `SuperDirt.start` in supercollider, ready for tidal.
-        superdirt-start = pkgs.writeShellScriptBin "superdirt-start" ''
-          ${pkgs.supercollider-with-plugins}/bin/sclang \
-            -l "${superdirt}/sclang_conf.yaml" \
-            ${pkgs.writeText "superdirt-start.sc" "SuperDirt.start;"}
-        '';
-
-        # Installs SuperDirt under your user's supercollider quarks.
-        superdirt-install = pkgs.writeShellScriptBin "superdirt-start" ''
-          ${pkgs.supercollider-with-plugins}/bin/sclang ${superdirt}/install.sc
-        '';
-
-        # Run the tidal interpreter (ghci running BootTidal.hs).
-        tidal = pkgs.writeShellScriptBin "tidal" ''
-          ${ghcWithTidal}/bin/ghci -ghci-script ${inputs.tidal-src}/BootTidal.hs
-        '';
-
-        # Vim plugin for tidalcycles.
-        vim-tidal = pkgs.vimUtils.buildVimPluginFrom2Nix {
-          pname = "vim-tidal";
-          version = "master";
-          src = inputs.vim-tidal-src;
-          meta = {
-            homepage = "https://github.com/tidalcycles/vim-tidal.vim";
-            license = pkgs.lib.licenses.mit;
-          };
-        };
+    system = "x86_64-linux";
+    quark-lib = import ./quark/lib.nix;
+    pkgs = inputs.nixpkgs.legacyPackages.${system};
+    ghcWithTidal = pkgs.haskellPackages.ghcWithPackages (p: [ p.tidal ]);
+  in rec {
+    packages.${system} = rec {
+      # SuperCollider quarks that are necessary for Tidal.
+      dirt-samples = pkgs.callPackage ./quark/dirt-samples.nix {
+        inherit (quark-lib) mkQuark;
+        inherit (inputs) dirt-samples-src;
+      };
+      vowel = pkgs.callPackage ./quark/vowel.nix {
+        inherit (quark-lib) mkQuark;
+        inherit (inputs) vowel-src;
+      };
+      superdirt = pkgs.callPackage ./quark/superdirt.nix {
+        inherit (quark-lib) mkQuark;
+        inherit (inputs) superdirt-src;
+        dependencies = [ dirt-samples vowel ];
       };
 
-      devShells.${system} = rec {
-        # A shell that provides a set of commonly useful packages for tidal.
-        tidal = pkgs.mkShell {
-          name = "tidal";
-          buildInputs = [
-            pkgs.supercollider-with-plugins
-            packages.${system}.superdirt-start
-            packages.${system}.tidal
-          ];
+      # Run `SuperDirt.start` in supercollider, ready for tidal.
+      superdirt-start = pkgs.writeShellScriptBin "superdirt-start" ''
+        ${pkgs.supercollider-with-plugins}/bin/sclang \
+          -l "${superdirt}/sclang_conf.yaml" \
+          ${pkgs.writeText "superdirt-start.sc" "SuperDirt.start;"}
+      '';
+
+      # Installs SuperDirt under your user's supercollider quarks.
+      superdirt-install = pkgs.writeShellScriptBin "superdirt-start" ''
+        ${pkgs.supercollider-with-plugins}/bin/sclang ${superdirt}/install.sc
+      '';
+
+      # Run the tidal interpreter (ghci running BootTidal.hs).
+      tidal = pkgs.writeShellScriptBin "tidal" ''
+        ${ghcWithTidal}/bin/ghci -ghci-script ${inputs.tidal-src}/BootTidal.hs
+      '';
+
+      # Vim plugin for tidalcycles.
+      vim-tidal = pkgs.vimUtils.buildVimPluginFrom2Nix {
+        pname = "vim-tidal";
+        version = "master";
+        src = inputs.vim-tidal-src;
+        meta = {
+          homepage = "https://github.com/tidalcycles/vim-tidal.vim";
+          license = pkgs.lib.licenses.mit;
         };
-        default = tidal;
       };
     };
-  in
-    out "x86_64-linux";
+
+    devShells.${system} = rec {
+      # A shell that provides a set of commonly useful packages for tidal.
+      tidal = pkgs.mkShell {
+        name = "tidal";
+        buildInputs = [
+          pkgs.supercollider-with-plugins
+          packages.${system}.superdirt-start
+          packages.${system}.tidal
+        ];
+      };
+      default = tidal;
+    };
+  };
 }
