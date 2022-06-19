21a353d9ad592ea202d68816ce76ccacecdf2190
mitchmindtree <mail@mitchellnordine.com>
2022-06-20 01:00:28 +1000

    Add a tidal project template flake
    Allows for initialising a new tidal project flake with:
    
    ```
    nix flake new --template github:mitchmindtree/tidalcycles.nix ./my-tidal-project
    ```

--- template/flake.nix
+++ template/flake.nix
@@ -1,8 +1,11 @@
 {
-  description = ''
-    A Nix flake for working with Tidal Cycles. https://tidalcycles.org/
-  '';
-  inputs = {};
-  outputs = inputs: {};
-}
+  description = "A Tidal Cycles project. https://tidalcycles.org/";
 
+  inputs.tidal.url = "github:mitchmindtree/tidalcycles.nix";
+
+  outputs = inputs:
+    inputs.tidal.utils.eachSupportedSystem (system: {
+      devShells = inputs.tidal.devShells.${system};
+      formatter = inputs.tidal.formatter.${system};
+    });
+}
