f1e29fa9ccfa12d0e15f7e1b041b5c265eac7597
heyarne <arne@schlueter.is>
2022-09-18 21:44:27 +0200

    Add help flag and sanity check

--- flake.nix
+++ flake.nix
@@ -91,6 +91,22 @@
         runtimeInputs = [supercollider];
         text = ''
           start_script="''${1:-${superdirt-start-sc}}"
+
+          if [ "$start_script" == "-h" ] || [ "$start_script" == "--help" ]; then
+            echo "Usage: superdirt-start [script]"
+            echo
+            echo "Start superdirt, optionally running a custom start script."
+            echo
+            echo "Options:"
+            echo "  -h --help    Show this screen."
+            exit
+          fi
+
+          if [ ! -e "$start_script" ]; then
+            echo "The script \"$start_script\" does not exist, aborting."
+            exit 1
+          fi
+
           ${sclang-with-superdirt}/bin/sclang-with-superdirt "$start_script"
         '';
       };
