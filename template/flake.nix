883a2ac184f6584254c166462cd868591087d6d7
Pim Kunis <pim@kunis.nl>
2024-10-13 23:48:33 +0200

    Fix flake-utils usage in flake template

--- template/flake.nix
+++ template/flake.nix
@@ -1,11 +1,14 @@
 {
   description = "A Tidal Cycles project. https://tidalcycles.org/";
 
-  inputs.tidal.url = "github:mitchmindtree/tidalcycles.nix";
+  inputs = {
+    tidal.url = "github:mitchmindtree/tidalcycles.nix";
+    utils.url = "github:numtide/flake-utils";
+  };
 
   outputs =
     inputs:
-    inputs.tidal.utils.eachSupportedSystem (system: {
+    inputs.utils.lib.eachDefaultSystem (system: {
       devShells = inputs.tidal.devShells.${system};
       formatter = inputs.tidal.formatter.${system};
     });
